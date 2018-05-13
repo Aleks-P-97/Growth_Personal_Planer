4fb2435a747333739c5634cabd4bb3ebc4f22315
Aleksandra Petkova <a.petkova2187@gmail.com>
2018-05-13 10:21:15 +0100

    Database created

---
+++ Growth/Growth/Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO; //Needed for files
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Growth
+{
+    class Database
+    {
+        //public SQLiteConnection myConnection;
+
+        //public Database()
+        //{
+        //    myConnection = new SQLiteConnection("Data Source=database.sqlite3");
+
+        //    if(!File.Exists("./database.sqlite3"))
+        //    {
+        //        SQLiteConnection.CreateFile("database.sqlite3");
+        //    }
+        //}
+
+    }
+}
